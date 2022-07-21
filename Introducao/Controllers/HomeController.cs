a94216954476cde4915de64d766245205a43916f
erley.vieira <erley.vieira@FBCEDESKTOP245>
2022-07-21 10:43:11 -0300

    Add project files.

---
+++ Introducao/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Introducao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Introducao.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa
+            {
+                PessoaId = 1,
+                Nome = "Erley Vieira",
+                Tipo = "Administrador"
+            };
+            
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public ActionResult Lista(Pessoa pessoa)
+        {
+            ViewData["PessoaId"] = pessoa.PessoaId;
+            ViewData["Nome"] = pessoa.Nome;
+            ViewData["Tipo"] = pessoa.Tipo;
+
+            return View(pessoa);
+        }
+
+        //public ActionResult ()
+        public ActionResult Contatos()
+        {
+            ViewBag.Message = "Contatos";
+
+            return View();
+        }
+        public ActionResult Sobre()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        
+
+
+
+    }
+}
---
+++ Introducao/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using Introducao.Models;
+using System.Web.Mvc;
+
+namespace Introducao.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            var usuario = new Usuario();
+
+            return View(usuario);
+        }
+
+        public ActionResult Index(Usuario usuario)
+        {
+            return View(usuario);
+        }
+    }
+}
